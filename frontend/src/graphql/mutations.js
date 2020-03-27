b7000a0af7abba5a47468501de5672aee9f5662a
Vijay Raavi <vraavi@calpoly.edu>
2020-03-26 23:33:30 -0700

    Edited schema to add @connection between students and tutors for session

---
+++ frontend/src/graphql/mutations.js
@@ -0,0 +1,300 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createStudent = /* GraphQL */ `
+  mutation CreateStudent(
+    $input: CreateStudentInput!
+    $condition: ModelStudentConditionInput
+  ) {
+    createStudent(input: $input, condition: $condition) {
+      id
+      name
+      email
+      cell
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const updateStudent = /* GraphQL */ `
+  mutation UpdateStudent(
+    $input: UpdateStudentInput!
+    $condition: ModelStudentConditionInput
+  ) {
+    updateStudent(input: $input, condition: $condition) {
+      id
+      name
+      email
+      cell
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const deleteStudent = /* GraphQL */ `
+  mutation DeleteStudent(
+    $input: DeleteStudentInput!
+    $condition: ModelStudentConditionInput
+  ) {
+    deleteStudent(input: $input, condition: $condition) {
+      id
+      name
+      email
+      cell
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const createTutor = /* GraphQL */ `
+  mutation CreateTutor(
+    $input: CreateTutorInput!
+    $condition: ModelTutorConditionInput
+  ) {
+    createTutor(input: $input, condition: $condition) {
+      id
+      name
+      email
+      cell
+      classes {
+        id
+        dept
+        num
+      }
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const updateTutor = /* GraphQL */ `
+  mutation UpdateTutor(
+    $input: UpdateTutorInput!
+    $condition: ModelTutorConditionInput
+  ) {
+    updateTutor(input: $input, condition: $condition) {
+      id
+      name
+      email
+      cell
+      classes {
+        id
+        dept
+        num
+      }
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const deleteTutor = /* GraphQL */ `
+  mutation DeleteTutor(
+    $input: DeleteTutorInput!
+    $condition: ModelTutorConditionInput
+  ) {
+    deleteTutor(input: $input, condition: $condition) {
+      id
+      name
+      email
+      cell
+      classes {
+        id
+        dept
+        num
+      }
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const createSession = /* GraphQL */ `
+  mutation CreateSession(
+    $input: CreateSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    createSession(input: $input, condition: $condition) {
+      id
+      studentID
+      tutorID
+      class {
+        id
+        dept
+        num
+      }
+      student {
+        id
+        name
+        email
+        cell
+        sessions {
+          nextToken
+        }
+      }
+      tutor {
+        id
+        name
+        email
+        cell
+        classes {
+          id
+          dept
+          num
+        }
+        sessions {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const updateSession = /* GraphQL */ `
+  mutation UpdateSession(
+    $input: UpdateSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    updateSession(input: $input, condition: $condition) {
+      id
+      studentID
+      tutorID
+      class {
+        id
+        dept
+        num
+      }
+      student {
+        id
+        name
+        email
+        cell
+        sessions {
+          nextToken
+        }
+      }
+      tutor {
+        id
+        name
+        email
+        cell
+        classes {
+          id
+          dept
+          num
+        }
+        sessions {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const deleteSession = /* GraphQL */ `
+  mutation DeleteSession(
+    $input: DeleteSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    deleteSession(input: $input, condition: $condition) {
+      id
+      studentID
+      tutorID
+      class {
+        id
+        dept
+        num
+      }
+      student {
+        id
+        name
+        email
+        cell
+        sessions {
+          nextToken
+        }
+      }
+      tutor {
+        id
+        name
+        email
+        cell
+        classes {
+          id
+          dept
+          num
+        }
+        sessions {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const createClass = /* GraphQL */ `
+  mutation CreateClass(
+    $input: CreateClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    createClass(input: $input, condition: $condition) {
+      id
+      dept
+      num
+    }
+  }
+`;
+export const updateClass = /* GraphQL */ `
+  mutation UpdateClass(
+    $input: UpdateClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    updateClass(input: $input, condition: $condition) {
+      id
+      dept
+      num
+    }
+  }
+`;
+export const deleteClass = /* GraphQL */ `
+  mutation DeleteClass(
+    $input: DeleteClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    deleteClass(input: $input, condition: $condition) {
+      id
+      dept
+      num
+    }
+  }
+`;
---
+++ frontend/src/graphql/subscriptions.js
@@ -0,0 +1,264 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateStudent = /* GraphQL */ `
+  subscription OnCreateStudent {
+    onCreateStudent {
+      id
+      name
+      email
+      cell
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const onUpdateStudent = /* GraphQL */ `
+  subscription OnUpdateStudent {
+    onUpdateStudent {
+      id
+      name
+      email
+      cell
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const onDeleteStudent = /* GraphQL */ `
+  subscription OnDeleteStudent {
+    onDeleteStudent {
+      id
+      name
+      email
+      cell
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const onCreateTutor = /* GraphQL */ `
+  subscription OnCreateTutor {
+    onCreateTutor {
+      id
+      name
+      email
+      cell
+      classes {
+        id
+        dept
+        num
+      }
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const onUpdateTutor = /* GraphQL */ `
+  subscription OnUpdateTutor {
+    onUpdateTutor {
+      id
+      name
+      email
+      cell
+      classes {
+        id
+        dept
+        num
+      }
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const onDeleteTutor = /* GraphQL */ `
+  subscription OnDeleteTutor {
+    onDeleteTutor {
+      id
+      name
+      email
+      cell
+      classes {
+        id
+        dept
+        num
+      }
+      sessions {
+        items {
+          id
+          studentID
+          tutorID
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const onCreateSession = /* GraphQL */ `
+  subscription OnCreateSession {
+    onCreateSession {
+      id
+      studentID
+      tutorID
+      class {
+        id
+        dept
+        num
+      }
+      student {
+        id
+        name
+        email
+        cell
+        sessions {
+          nextToken
+        }
+      }
+      tutor {
+        id
+        name
+        email
+        cell
+        classes {
+          id
+          dept
+          num
+        }
+        sessions {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const onUpdateSession = /* GraphQL */ `
+  subscription OnUpdateSession {
+    onUpdateSession {
+      id
+      studentID
+      tutorID
+      class {
+        id
+        dept
+        num
+      }
+      student {
+        id
+        name
+        email
+        cell
+        sessions {
+          nextToken
+        }
+      }
+      tutor {
+        id
+        name
+        email
+        cell
+        classes {
+          id
+          dept
+          num
+        }
+        sessions {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const onDeleteSession = /* GraphQL */ `
+  subscription OnDeleteSession {
+    onDeleteSession {
+      id
+      studentID
+      tutorID
+      class {
+        id
+        dept
+        num
+      }
+      student {
+        id
+        name
+        email
+        cell
+        sessions {
+          nextToken
+        }
+      }
+      tutor {
+        id
+        name
+        email
+        cell
+        classes {
+          id
+          dept
+          num
+        }
+        sessions {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const onCreateClass = /* GraphQL */ `
+  subscription OnCreateClass {
+    onCreateClass {
+      id
+      dept
+      num
+    }
+  }
+`;
+export const onUpdateClass = /* GraphQL */ `
+  subscription OnUpdateClass {
+    onUpdateClass {
+      id
+      dept
+      num
+    }
+  }
+`;
+export const onDeleteClass = /* GraphQL */ `
+  subscription OnDeleteClass {
+    onDeleteClass {
+      id
+      dept
+      num
+    }
+  }
+`;
